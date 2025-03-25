e5450272fea3eb4f04e873969de5e3a1f40c17fc
Dor-J <Mr.DorJosef@gmail.com>
2025-03-25 18:29:16 +0200

    Add mouse move componenet logic

--- src/app/mouse-move/mouse-move.component.ts
+++ src/app/mouse-move/mouse-move.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mouse-move',
   standalone: false,
   templateUrl: './mouse-move.component.html',
-  styleUrl: './mouse-move.component.scss'
+  styleUrl: './mouse-move.component.scss',
 })
 export class MouseMoveComponent {
+  isOn: boolean = true;
+  currMousePosition: {
+    x: number;
+    y: number;
+  } = { x: 0, y: 0 };
 
+  @HostListener('window:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (this.isOn) {
+      this.currMousePosition = {
+        ...this.currMousePosition,
+        x: event.screenX,
+        y: event.screenY,
+      };
+    }
+  }
+
+  onToggleIsOn(): void {
+    this.isOn = !this.isOn;
+  }
 }
